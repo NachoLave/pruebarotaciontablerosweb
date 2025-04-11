b472856e95a77f3028ff9e2b1551084dfd96eb6f
Ignaciolavezzari <110949438+NachoLave@users.noreply.github.com>
2025-04-10 21:39:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const LINKS = [
-    "https://app.powerbi.com/groups/me/reports/7e9d3785-...1e39?chromeless=true&experience=power-bi",
-    "https://app.powerbi.com/groups/me/reports/88b3d9cb-...3402?chromeless=true&experience=power-bi"
+  "https://app.powerbi.com/groups/647f43d9-54a1-47ee-9e78-604bb84ac9ff/reports/01661e10-0139-4bee-bf88-1b96ba9d7f2e/44f3f26abe0a6fb92b24?experience=power-bi&chromeless=true", //Empleados - Altas y bajas
+        "https://app.powerbi.com/groups/647f43d9-54a1-47ee-9e78-604bb84ac9ff/reports/01661e10-0139-4bee-bf88-1b96ba9d7f2e/ReportSectionf9fe292b4b0b2244a793?experience=power-bi&chromeless=true", //Edad y Sexo
+        "https://app.powerbi.com/groups/647f43d9-54a1-47ee-9e78-604bb84ac9ff/reports/01661e10-0139-4bee-bf88-1b96ba9d7f2e/ReportSection7b34db0f300b875c8c1b?experience=power-bi&chromeless=true", //Antiguedad
+"https://app.powerbi.com/groups/647f43d9-54a1-47ee-9e78-604bb84ac9ff/reports/01661e10-0139-4bee-bf88-1b96ba9d7f2e/ReportSection1554dd7e053a66979e0b?experience=powerbi&chromeless=true", //empleados Dotacion
 ];
 
 const ROTATION_INTERVAL = 20000; // 20 segundos
